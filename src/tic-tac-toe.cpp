0302f45fbb9bc7325e63cb1d0deda883ed282768
LAPTOP-G82ED60A\User <petrova.a@panoramik.ru>
2023-09-28 00:53:50 +0300

    initial commit

---
+++ src/tic-tac-toe.cpp
@@ -0,0 +1,12 @@
+﻿// tic-tac-toe.cpp: определяет точку входа для приложения.
+//
+
+#include "tic-tac-toe.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
