31b86fed8fe4f6da92a4a6b9c9365dc015e7b7b5
ekargalov <146271383+ekargalov@users.noreply.github.com>
2023-10-11 02:01:18 +0300

    add a new class player (#3)
    Add a new class player

--- src/tic-tac-toe.cpp
+++ src/tic-tac-toe.cpp
@@ -3,7 +3,31 @@
 
 #include "tic-tac-toe.h"
 
+#include <vector>
+#include <string>
+
 using namespace std;
+
+class Player {
+public:
+    Player(string name, bool isCross)
+    : d_name(name),
+    d_isCross(isCross)
+    {    
+    }
+
+    string getName() {
+        return d_name;
+    }
+
+    bool getIsCross() {
+        return d_isCross;
+    }
+
+private:
+    string d_name;
+    bool d_isCross;
+};
 
 void clearScreen() {
     system("cls");
@@ -12,6 +36,7 @@ void clearScreen() {
 int main()
 {
     string name1, name2;
+
     cout << "Enter the name of first player:\n";
     cin >> name1;
     clearScreen();
@@ -20,7 +45,10 @@ int main()
     cin >> name2;
     clearScreen();
 
-    cout << "Welcome " << name1 << " and " << name2;
-	return 0;
+    Player player1(name1, true);
+    Player player2(name2, false);
+
+    cout << "Welcome " << player1.getName() << " and " << player2.getName();
+    return 0;
 }
 	
