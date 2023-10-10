a5cfec98a34fe6a43a42f55a086ce0de3acd7010
LAPTOP-G82ED60A\User <petrova.a@panoramik.ru>
2023-10-10 19:12:27 +0300

    add a new class player

--- src/tic-tac-toe.cpp
+++ src/tic-tac-toe.cpp
@@ -5,6 +5,39 @@
 
 using namespace std;
 
+/* class Field {
+public:
+    Field (int n, int m) {
+        
+    }
+
+    bool makeMove(Player* player, int x, int y) {
+        
+    }
+
+    int status(Player* player) { // 2 -- win, -2 -- fail, 0 -- the game in progress 1 -- draw, -1 -- other;
+        
+    }
+
+private:
+    vector<vector<Player*>> d_grid;
+}; */
+
+class Player {
+public:
+    Player(string p_name, bool p_isCross) {
+        name = p_name;
+        isCross = p_isCross;
+    }
+
+    string getName() {
+        return name;
+    }
+private:
+    string name;
+    bool isCross;
+};
+
 void clearScreen() {
     system("cls");
 }
@@ -12,6 +45,7 @@ void clearScreen() {
 int main()
 {
     string name1, name2;
+
     cout << "Enter the name of first player:\n";
     cin >> name1;
     clearScreen();
@@ -20,7 +54,11 @@ int main()
     cin >> name2;
     clearScreen();
 
-    cout << "Welcome " << name1 << " and " << name2;
-	return 0;
+    // cout << "Welcome " << name1 << " and " << name2;
+
+    Player player1(name1, true);
+    Player player2(name2, false);
+    cout << player1.getName() << " " << player2.getName();
+    return 0;
 }
 	
--- src/tic-tac-toe.h
+++ src/tic-tac-toe.h
@@ -4,5 +4,7 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
+#include <string>
 
 // TODO: установите здесь ссылки на дополнительные заголовки, требующиеся для программы.
