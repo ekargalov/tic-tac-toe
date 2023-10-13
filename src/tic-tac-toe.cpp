fecece39d386584159f7d131eef94b1ea8130f85
LAPTOP-G82ED60A\User <petrova.a@panoramik.ru>
2023-10-13 17:56:11 +0300

    Add a new class field

--- src/tic-tac-toe.cpp
+++ src/tic-tac-toe.cpp
@@ -8,6 +8,8 @@
 
 using namespace std;
 
+enum class Status {fail = -2, other, inProgress, draw, win};
+
 class Player {
 public:
     Player(string name, bool isCross)
@@ -20,13 +22,50 @@ public:
         return d_name;
     }
 
-    bool getIsCross() {
+    bool isCross() {
         return d_isCross;
     }
 
 private:
     string d_name;
     bool d_isCross;
+};
+
+class Field {
+public:
+    Field (int n) {
+        for (int i = 1; i <= n; ++i) {
+            d_grid.push_back({});
+            for (int j = 1; j <= n; ++j) {
+                d_grid.back().push_back(nullptr);
+            }
+        }
+    }
+
+    bool makeMove(const Player& player, int x, int y) {
+        if (d_grid[x][y] == nullptr) {
+            d_grid[x][y] = &player;
+            return true;
+        }
+        return false;
+    }
+
+    Status status(const Player& player) { // 2 -- win, -2 -- fail, 0 -- the game in progress 1 -- draw, -1 -- other;
+        for (int i = 1; i <= d_grid.size(); ++i) {
+            for (int j = 1; j <= d_grid.size(); ++j) {
+                if (d_grid[i][j] == nullptr) {
+                    return inProgress;
+                }
+                else {
+                    return draw;
+                }
+            }
+        }
+    }
+    
+private:
+    using enum Status;
+    vector<vector<const Player*>> d_grid;
 };
 
 void clearScreen() {
